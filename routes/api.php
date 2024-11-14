fa56fbca1ed015a5c9917d7a77f9c94064fa9695
Dragon <115075047+Dragonbestgg1@users.noreply.github.com>
2024-11-14 14:52:48 +0200

    .....

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,12 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('auth/google', [AuthController::class, 'redirectToGoogle']);
+
 Route::get('auth/google/callback', [AuthController::class, 'handleGoogleCallback']);
+
+
